1b181157117a5465232cfd9f2f65ee72733988cc
Vanessatpm <77583397+Vanessatpm@users.noreply.github.com>
2023-02-21 16:21:22 +0100

    add GetMostPopularGenreFor

--- DataAccessWithSQLClient/DataAccessWithSQLClient/Repositories/Customers/ICustomerRepository.cs
+++ DataAccessWithSQLClient/DataAccessWithSQLClient/Repositories/Customers/ICustomerRepository.cs
@@ -12,6 +12,7 @@ namespace DataAccessWithSQLClient.Repositories.Customers
         List<Customer> GetPage(int limit, int offset);
         List<Customer> GetByName(string name);
         List<CustomerCountry> GetAllCustomerCountriesDescending();
+        List<CustomerGenre> GetMostPopularGenreFor(Customer customer);
 
     }
 }
