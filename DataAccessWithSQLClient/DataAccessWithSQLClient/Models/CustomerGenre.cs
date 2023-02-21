a2b31197942d189bfa556b943a884ec1f2eb8266
Vanessatpm <77583397+Vanessatpm@users.noreply.github.com>
2023-02-21 16:17:54 +0100

    feat, wip: Add model CustomerGenre

---
+++ DataAccessWithSQLClient/DataAccessWithSQLClient/Models/CustomerGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessWithSQLClient.Models
+{
+    internal readonly record struct CustomerGenre(
+        Customer customer,
+        string genre
+        );
+    
+}
